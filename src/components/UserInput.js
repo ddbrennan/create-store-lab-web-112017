a4e1782687f32aa109985943898f50ebb14bfc2d
ddbrennan <ddbren@gmail.com>
2018-02-19 18:11:33 -0500

    store created

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  state = {
+    username: "",
+    hometown: ""
+  }
+
+  changeForm = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  submitForm = (e) => {
+    e.preventDefault()
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: {
+        username: this.state.username,
+        hometown: this.state.hometown
+      }
+    })
+    this.setState({
+      username: "",
+      hometown: ""
+    })
+  }
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={this.submitForm}>
+          <input onChange={this.changeForm} value={this.state.hometown} name="hometown" type="text"></input>
+          <input onChange={this.changeForm} value={this.state.username} name="username" type="text"></input>
+          <input type="submit"></input>
+        </form>
       </div>
     );
   }
